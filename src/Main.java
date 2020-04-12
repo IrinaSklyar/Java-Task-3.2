b0126168ff7a3c7cbd9dd5c5531120764de7f967
Irina Skliarova <i.skliarova2015@yandex.ru>
2020-04-12 10:11:00 +0300

    Initial cmmit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+               int payment = 1100;
+               int bonus = 0;
+                if (payment>1000) {
+                    bonus = payment / 100;
+        }
+        System.out.println (bonus);
+    }
+}
