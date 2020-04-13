f9d28252b3177919df19276bff29a316875bcf4d
Irina Skliarova <i.skliarova2015@yandex.ru>
2020-04-13 18:04:11 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-               int payment = 1100;
-               int bonus = 0;
-                if (payment>1000) {
-                    bonus = payment / 100;
+        int payment = 1100;
+        int bonus = 0;
+        if (payment > 1000) {
+            bonus = payment / 100;
         }
-        System.out.println (bonus);
+        System.out.println(bonus);
     }
 }
